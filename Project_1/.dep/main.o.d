d08f4b42600d57c2587ba02ba9bc96707b4ae620
yzahir <youssef50zouhayr@gmail.com>
2025-05-05 22:58:52 +0200

    task2_project1

--- Project_1/.dep/main.o.d
+++ Project_1/.dep/main.o.d
@@ -138,7 +138,12 @@ build/obj/main.o: main.c \
  c:\users\youss\desktop\study\sos25\distributed_ro_lab\myrepo\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\unistd.h \
  ../e-puck2_main-processor/src/parameter/parameter.h \
  ../e-puck2_main-processor/src/leds.h \
- ../e-puck2_main-processor/src/spi_comm.h
+ ../e-puck2_main-processor/src/spi_comm.h \
+ ../e-puck2_main-processor/src/sensors/proximity.h \
+ ../e-puck2_main-processor/src/selector.h \
+ ../e-puck2_main-processor/src/motors.h \
+ ../e-puck2_main-processor/ChibiOS//os/hal/lib/streams/chprintf.h \
+ ../e-puck2_main-processor/src/usbcfg.h
 
 c:\users\youss\desktop\study\sos25\distributed_ro_lab\myrepo\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\stdio.h:
 
@@ -421,3 +426,13 @@ c:\users\youss\desktop\study\sos25\distributed_ro_lab\myrepo\eclipse_e-puck2\too
 ../e-puck2_main-processor/src/leds.h:
 
 ../e-puck2_main-processor/src/spi_comm.h:
+
+../e-puck2_main-processor/src/sensors/proximity.h:
+
+../e-puck2_main-processor/src/selector.h:
+
+../e-puck2_main-processor/src/motors.h:
+
+../e-puck2_main-processor/ChibiOS//os/hal/lib/streams/chprintf.h:
+
+../e-puck2_main-processor/src/usbcfg.h:
--- Project_1/main.c
+++ Project_1/main.c
@@ -2,39 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
 #include <main.h>
 #include "leds.h"
 #include "spi_comm.h"
+#include "sensors/proximity.h"
+#include "selector.h"
+#include "motors.h"
+#include "chprintf.h"
+#include "usbcfg.h"
 
 
 int main(void)
 {
-
+	usb_start();
     halInit();
     chSysInit();
     mpu_init();
+    motors_init();
+    clear_leds();
+    spi_comm_start();
 
-    // initialisation
-    //set_led(LED2 , 2);
-    //set_rgb_led(LED4, 1, 2, 4);
-    set_front_led(2);
-
-
-
+    int sel = 0;
+    int direction =1;
     /* Infinite loop. */
     while (1) {
-    	//waits 1 second
-        chThdSleepMilliseconds(1000);
+    	left_motor_set_speed(direction *350);
+    	right_motor_set_speed(direction*-350);
+
+    	chThdSleepMilliseconds(500);
+    	set_body_led(1);
+    	chThdSleepMilliseconds(500);
+    	set_body_led(0);
+    	sel++;
+
+        if (get_selector() != 0 && sel >= get_selector()) {
+        	direction *= -1;
+        	sel = 0;
+        }
+
     }
 }
-
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
-
 void __stack_chk_fail(void)
 {
     chSysHalt("Stack smashing detected");
